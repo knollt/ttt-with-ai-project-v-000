b0545bcbd09fa6b4d1739a9c8d4ff3156cb630a6
Tim Knoll <timothy.knoll@gmail.com>
2019-04-05 00:41:08 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,5 +1,19 @@
 module Players
   class Player::Computer < Player
+    attr_accessor :board
+
+    WIN_COMBINATIONS = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+      ]
+
+      CORNERS = [0,2,6,8]
 
     def move(board)
       if board.cells[4] == ' '
