38864ccce65906eea6376d01f05bd915b32592d3
Tim Knoll <timothy.knoll@gmail.com>
2019-04-05 00:46:47 +0000

    Automatically backed up by Learn

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -3,7 +3,7 @@ require 'pry'
 module Players
   class Player::Human < Player
 
-    def move(board, timer=0)
+    def move(board)
       puts "Please enter your selection 1-9"
       gets.chomp
     end
